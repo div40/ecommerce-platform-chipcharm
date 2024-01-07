634623d1788f87f6320b1393caaaa9922eae37e8
div40 <vidh40@gmail.com>
2024-01-07 20:52:51 +0530

    security stuff

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -1,4 +1,15 @@
-import { CollectionConfig } from "payload/types";
+import { Access, CollectionConfig } from "payload/types";
+
+
+const adminsAndUser: Access = ({req: {user}}) => {
+    if(user.role === "admin") return true
+
+    return {
+        id: {
+            equals: user.id
+        }
+    }
+}
 
 export const Users: CollectionConfig = {
     slug: "users",
@@ -10,10 +21,26 @@ export const Users: CollectionConfig = {
         }
     },
     access: {
-        read: () => true,
+        read: adminsAndUser,
         create: () => true,
+        update: ({req}) => req.user.role === "admin",
+        delete: ({req}) => req.user.role === "admin",
+    },
+    admin: {
+        hidden: ({user}) => user.role !== "admin",
+        defaultColumns: ["id"],
     },
     fields: [
+        {
+            name: "products",
+            label: "Products",
+            admin: {
+                condition: () => false
+            },
+            type: "relationship",
+            relationTo: "products",
+            hasMany: true
+        },
         {
             name: "role",
             defaultValue: "user",
